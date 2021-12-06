ffa572d0e11801fa6afe7ca253f8c0c5546f608e
Gianna Smith <gnsmith127@gmail.com>
2021-12-06 15:36:12 -0500

    Completed Operators Excercises 1 and 2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var calculateArea = AreaOfCircle(radius);
+
+            Console.WriteLine($"The area of a circle with radius of {radius} is {calculateArea}.");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            
+            return Math.PI * radius * radius;
+            
+        }
+    }
+}
